825e5debc09468f77eab29523ecc8eeb91f8537f
stevenpfaff <pfaffy1426@gmail.com>
2021-11-03 10:19:23 -0500

    test

---
+++ drf_jwt_capstone_backend/drf_jwt_capstone_backend/local_settings.py
@@ -0,0 +1,19 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-2#se2w2t^20h%7g6_6+(zztxlmz#99f*r(dgsri7@ip_8zov@'
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'dew_cup_database',
+        'USER': 'root',
+        'PASSWORD': 'stammer91',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
