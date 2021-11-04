f825a3cc192e5719ffe44fc18f62938b7637ca3d
stevenpfaff <pfaffy1426@gmail.com>
2021-11-04 13:53:09 -0500

    added teams url

--- drf_jwt_capstone_backend/drf_jwt_capstone_backend/urls.py
+++ drf_jwt_capstone_backend/drf_jwt_capstone_backend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/players/', include('players.urls')),
+    path('api/teams/', include('teams.urls')),
 ]
