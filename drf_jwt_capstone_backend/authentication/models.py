4e0398e2be6fc61ed15933a9a2528ca39d0181de
stevenpfaff <pfaffy1426@gmail.com>
2021-11-04 13:16:24 -0500

    added is_captain to auth

--- drf_jwt_capstone_backend/authentication/models.py
+++ drf_jwt_capstone_backend/authentication/models.py
@@ -12,3 +12,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    is_captain = models.BooleanField(default=False)
