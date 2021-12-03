3a4cad9998dc8b0d1c39ae6bd5d66c6f8132cdfd
stevenpfaff <pfaffy1426@gmail.com>
2021-12-03 10:56:05 -0600

    added baseball categories to teams

--- drf_jwt_capstone_backend/teams/models.py
+++ drf_jwt_capstone_backend/teams/models.py
@@ -11,4 +11,8 @@ class Team(models.Model):
     championships = models.IntegerField()
     players = models.CharField(max_length=100)
     file = models.TextField()
+    runs = models.IntegerField()
+    runs_against = models.IntegerField()
+    homeruns = models.IntegerField()
+    batting_average = models.DecimalField(max_digits=3, decimal_places=3)
     
--- drf_jwt_capstone_backend/teams/serializers.py
+++ drf_jwt_capstone_backend/teams/serializers.py
@@ -3,4 +3,4 @@ from .models import Team
 class TeamSerialzer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['name', 'wins', 'losses', 'goals', 'goals_against', 'championships', 'players', 'file']
+        fields = ['name', 'wins', 'losses', 'goals', 'goals_against', 'championships', 'players', 'file', 'runs', 'runs_against', 'homeruns', 'batting_average']
