c08bb01b1cf871bd8d1196e291ee856243c2e946
stevenpfaff <pfaffy1426@gmail.com>
2021-12-05 23:06:23 -0600

    added game type

--- drf_jwt_capstone_backend/games/serializers.py
+++ drf_jwt_capstone_backend/games/serializers.py
@@ -3,4 +3,4 @@ from .models import Game
 class GameSerialzer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = [ 'game', 'home_team', 'home_score', 'home_players', 'away_team', 'away_score', 'away_players']
+        fields = [ 'game', 'type', 'home_team', 'home_score', 'home_players', 'away_team', 'away_score', 'away_players']
